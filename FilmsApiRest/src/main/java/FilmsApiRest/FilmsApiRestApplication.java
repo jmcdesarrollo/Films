df0923e7f36a6b9396ecdee42118795fe9d16f81
Javier Medina Cabrerizo <158183834+jmcdesarrollo@users.noreply.github.com>
2024-05-09 09:11:51 +0200

    Init Commit
    ApiRest

---
+++ FilmsApiRest/src/main/java/FilmsApiRest/FilmsApiRestApplication.java
@@ -0,0 +1,13 @@
+package FilmsApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsApiRestApplication.class, args);
+	}
+
+}
---
+++ FilmsApiRest/src/test/java/FilmsApiRest/FilmsApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package FilmsApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
